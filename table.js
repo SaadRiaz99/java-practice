5067f905d4902be0a387b4f58d121bbcf0a4d252
SaadRiaz99 <140387256+SaadRiaz99@users.noreply.github.com>
2026-01-22 23:13:35 +0500

    Thora error hai sahi karna hai

--- table.js
+++ table.js
@@ -4,9 +4,12 @@ i = 1
 for (let i=1 ; i<=10 ; i++  ){
     console.log(`${num} * ${i} = ${num*i}`)
 }
-i=1
+
+let result = "";
+let i=1
 while (i<=11) {
     console.log(num+  "x" + i + "=" + (num*i))
     i++
     
 }
+document.getElementById("output").innerHTML  = result;
