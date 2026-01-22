8932daa154dc0513e818594756da269497e665cb
SaadRiaz99 <140387256+SaadRiaz99@users.noreply.github.com>
2026-01-22 23:01:59 +0500

    using loop add table for and while

---
+++ table.js
@@ -0,0 +1,12 @@
+const num  = prompt("Enter a number  you want to Table ")
+
+i = 1
+for (let i=1 ; i<=10 ; i++  ){
+    console.log(`${num} * ${i} = ${num*i}`)
+}
+i=1
+while (i<=11) {
+    console.log(num+  "x" + i + "=" + (num*i))
+    i++
+    
+}
