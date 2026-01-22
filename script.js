25637f0eedb55084bfea2ddb31f316df89b99711
SaadRiaz99 <140387256+SaadRiaz99@users.noreply.github.com>
2026-01-22 19:23:47 +0500

    loops and calculating

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log("Saad Bin RIaz");
+// a = "Saad"
+// console.log(a)
+// Object
+let student = {
+    name : "saad" ,
+    Age : 19 ,
+    Launguage : "Punjabi" ,
+};
+console.log(student)
+// calulator like python in java
+let a = 2;
+let b = 5;
+
+let c = a + b 
+console.log(a + b)
+
+// if else
+let age = prompt("Enter Your Age")       
+if (age >= 18 ){
+    console.log("You can Vote it ")
+}
+else{
+    console.log("Beta abhi to chota hai ")
+}
