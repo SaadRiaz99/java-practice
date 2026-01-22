2ebce76d1836ec565789e3926f4f201f36324720
SaadRiaz99 <140387256+SaadRiaz99@users.noreply.github.com>
2026-01-22 22:32:01 +0500

    Grade

---
+++ script2.js
@@ -0,0 +1,17 @@
+a = prompt("Enter Your Grade(A+ , B , C , D)");
+
+if (a  == "A+"){
+    console.log("to genius bacha hai")
+}
+else if (a  == "B"){
+    console.log("to Avg bacha hai")
+}
+else if (a  == "C"){
+    console.log("to legend bacha hai")
+}
+else if (a  == "D"){
+    console.log("to ultra pro legend bacha hai")
+}
+else {
+    console.log("Sahi Grade daal bache")
+}
